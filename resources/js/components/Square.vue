a3fea7f4abc26c378bef9b61cb2a1ad0ca22901d
noatel <noahtelussa@gmail.com>
2025-02-12 20:31:13 +0100

    inital commit

---
+++ resources/js/components/Square.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="max-w-md mx-auto lg:max-w-none relative mt-12 w-full max-w-[800px] lg:max-w-[700px] mx-auto"
+  >
+    <svg
+      class="w-full h-auto"
+      viewBox="0 0 1000 1000"
+      xmlns="http://www.w3.org/2000/svg"
+      id="square"
+    >
+      <rect
+        transform="translate(0 0) rotate(5) skewX(0) skewY(0) scale(1)"
+        transform-origin="0 0"
+        x="300"
+        y="0"
+        width="500"
+        height="500"
+        fill="#3182ce"
+      ></rect>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Square",
+};
+</script>
+
+<style scoped>
+/* Add any specific styles for the Square component here */
+</style>
