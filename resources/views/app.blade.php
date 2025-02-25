293de76f5fa7f4e1d4f3a7f374eeea24277c8a8d
noatel <noahtelussa@gmail.com>
2025-02-25 17:17:59 +0100

    Added favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,16 @@
     <meta name="robots" content="index, follow">
 
 
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}">
+    <meta name="theme-color" content="#ffffff">
+
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
